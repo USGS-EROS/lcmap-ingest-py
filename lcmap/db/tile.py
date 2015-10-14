9f4cc0bdf30211a4877274029fe7a8ee95e10b95
Duncan McGreggor <oubiwann@gmail.com>
2015-10-14 17:45:18 -0500

    Updated query functions to use new schema (with ubid).

--- lcmap/db/tile.py
+++ lcmap/db/tile.py
@@ -9,7 +9,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-SAVE_CQL = """INSERT INTO epsg_5070 (x, y, layer, source, acquired, data,
+SAVE_CQL = """INSERT INTO epsg_5070 (x, y, ubid, source, acquired, data,
               data_type, data_fill, data_range, data_scale, data_shape)
     VALUES (?,?,?,?,?,?,
             ?,?,?,?,?)"""
@@ -17,37 +17,37 @@ SAVE_CQL = """INSERT INTO epsg_5070 (x, y, layer, source, acquired, data,
 SAVE = session.prepare(SAVE_CQL)
 
 
-def save(x, y, layer, source, acquired, data, data_type, data_fill, data_range, data_scale, data_shape, **kwargs):
+def save(x, y, ubid, source, acquired, data, data_type, data_fill, data_range, data_scale, data_shape, **kwargs):
     """Save"""
     # logger.debug("Saving <%s,%s> (%s:%s) @ %s - %s" %
-    #              (x, y, layer, source, acquired, data_type))
-    parameters = (x, y, layer, source, acquired, data,
+    #              (x, y, ubid, source, acquired, data_type))
+    parameters = (x, y, ubid, source, acquired, data,
                   data_type, data_fill, data_range, data_scale, data_shape)
     return session.execute(SAVE, parameters)
 
 
 FIND_CQL = """SELECT * FROM epsg_5070 WHERE
-    x = ? AND y = ? AND layer = ? AND acquired > ? AND acquired < ?"""
+    x = ? AND y = ? AND ubid = ? AND acquired > ? AND acquired < ?"""
 
 FIND = session.prepare(FIND_CQL)
 
 
-def find(x, y, layer, t1, t2):
+def find(x, y, ubid, t1, t2):
     """Find a tile containing x, y.
 
     This does not post-process results. It returns a list of rows results
     that can be further processed. This means that the data blob isn't even
     a usable array.
     """
-    logger.debug("find <%s,%s> (%s) @ %s-%s." % (layer, x, y, t1, t2))
+    logger.debug("find <%s,%s> (%s) @ %s-%s." % (ubid, x, y, t1, t2))
     t1 = datetime.strptime(t1, "%Y-%m-%d")
     t2 = datetime.strptime(t2, "%Y-%m-%d")
-    parameters = (x, y, layer, t1, t2)
+    parameters = (x, y, ubid, t1, t2)
     results = session.execute(FIND, parameters)
     return results
 
 
-def find_area(x1, y1, x2, y2, layer, t1, t2, grid=30 * 100):
+def find_area(x1, y1, x2, y2, ubid, t1, t2, grid=30 * 100):
     """Find an area containing x1:x2, y1:y2.
 
     Like find, this function does not post-process results. This is a simple
@@ -61,6 +61,6 @@ def find_area(x1, y1, x2, y2, layer, t1, t2, grid=30 * 100):
     dtfmt = "%Y-%m-%d"
     t1, t2 = datetime.strptime(t1, dtfmt), datetime.strptime(t2, dtfmt)
 
-    args = [(tx, ty, layer, t1, t2) for ty in ys for tx in xs]
+    args = [(tx, ty, ubid, t1, t2) for ty in ys for tx in xs]
     results = execute_concurrent_with_args(session, tile.FIND, args)
     return results
