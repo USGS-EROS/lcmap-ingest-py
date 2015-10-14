4f6fb3d08650568863f2da420e38fd32d9d46de5
Duncan McGreggor <oubiwann@gmail.com>
2015-10-14 18:06:15 -0500

    Updated to use new tile size of 256.

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -24,30 +24,41 @@ def create(loc, a, off_x, off_y, pixel_size = 30):
 
 def make_geotiffs():
 
-    # We are assuming the tile grid points are evenly spaced multiples of 30 and 100.
-    tile_grid = 30 * 100
+    # We are assuming the tile grid points are evenly spaced multiples of 30
+    # and 256.
+    tile_grid = 30 * 256
 
-    # ...but we are going to create a relatively small tile. This will exercise filling
-    # data around an entire tile.
+    # ...but we are going to create a relatively small tile. This will exercise
+    # filling data around an entire tile.
     a = np.array(range(0,25))
     a = a.reshape(5,5)
 
     # this tiff is aligned to the grid
-    create("tests/data/tile_5x5/offset_0x_0y_30px.tif", a, -tile_grid*5, tile_grid*8)
+    create("tests/data/tile_5x5/offset_0x_0y_30px.tif", a, -tile_grid*5,
+            tile_grid*8)
 
     # this tiff is 2 grid-units below and to the right from the nearest tile.
-    create("tests/data/tile_5x5/offset_2x_2y_30px.tif", a, tile_grid+60, tile_grid-60)
+    create("tests/data/tile_5x5/offset_2x_2y_30px.tif", a, tile_grid+60,
+            tile_grid-60)
 
-    # this tiff is 3 grid units off along the x axis and 0 units off along the y axis.
-    create("tests/data/tile_5x5/offset_3x_0y_30px.tif", a, tile_grid+90, tile_grid)
+    # this tiff is 3 grid units off along the x axis and 0 units off along the
+    # y axis.
+    create("tests/data/tile_5x5/offset_3x_0y_30px.tif", a, tile_grid+90,
+            tile_grid)
 
-    # this tiff is 0 grid units off along the x axis and 3 units off along the y axis.
-    create("tests/data/tile_5x5/offset_0x_3y_30px.tif", a, tile_grid+90, tile_grid)
+    # this tiff is 0 grid units off along the x axis and 3 units off along the
+    # y axis.
+    create("tests/data/tile_5x5/offset_0x_3y_30px.tif", a, tile_grid+90,
+            tile_grid)
 
-    # this tiff is 0 grid units off along the x axis and 3 units off along the y axis.
-    create("tests/data/tile_5x5/offset_0x_0y_15px.tif", a, tile_grid, tile_grid, 15)
+    # this tiff is 0 grid units off along the x axis and 3 units off along the
+    # y axis.
+    create("tests/data/tile_5x5/offset_0x_0y_15px.tif", a, tile_grid,
+            tile_grid, 15)
 
-    # this tiff is 0 grid units off along the x axis and 3 units off along the y axis.
-    create("tests/data/tile_5x5/offset_0x10_0y20_30px.tif", a, tile_grid+10, tile_grid+20, 30)
+    # this tiff is 0 grid units off along the x axis and 3 units off along the
+    # y axis.
+    create("tests/data/tile_5x5/offset_0x10_0y20_30px.tif", a, tile_grid+10,
+            tile_grid+20, 30)
 
 make_geotiffs()
