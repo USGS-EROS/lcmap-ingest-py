ee257d99fa30255bd2660c61ea620d04b6792e8f
Duncan McGreggor <oubiwann@gmail.com>
2015-10-15 15:35:41 -0500

    Made .env file optional.

--- lcmap/config.py
+++ lcmap/config.py
@@ -1,7 +1,13 @@
 from os import environ
-from os.path import join, dirname
+import os.path
+
 from dotenv import load_dotenv
-load_dotenv('.env')
+
+
+dotenv_file = ".env"
+if os.path.exists(dotenv_file):
+    load_dotenv(dotenv_file)
+
 
 CASSANDRA_HOSTS = environ.get('CASSANDRA_HOSTS', 'localhost').split(',')
 CASSANDRA_PORT = int(environ.get('CASSANDRA_PORT', 9042))
@@ -11,5 +17,4 @@ CASSANDRA_PASS = environ.get('CASSANDRA_PASS', None)
 CASSANDRA_PROTOCOL_VERSION = int(environ.get('CASSANDRA_PROTOCOL_VERSION','4'))
 CASSANDRA_QUERY_CONCURRENCY = int(environ.get('CASSANDRA_QUERY_CONCURRENCY','100'))
 CASSANDRA_QUERY_TIMEOUT = int(environ.get('CASSANDRA_QUERY_TIMEOUT','5000'))
-
 LOG_LEVEL = environ.get('LOG_LEVEL')
